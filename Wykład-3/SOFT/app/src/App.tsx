4cb80d99f01b7a0bb3dd0b5bbe1a02d43d54aad6
kurdeno <46341978+kurdeno@users.noreply.github.com>
2021-04-26 18:23:34 +0200

    Projekt z wykładu

---
+++ Wykład-3/SOFT/app/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Switch ,Route,BrowserRouter} from 'react-router-dom';
+
+import Home from './views/home/Home';
+import Movie from './views/movie/Movie';
+import Page404 from './views/page404/Page404';
+
+import './App.css';
+
+
+const App = () => {
+  
+
+  
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movie/:id" component={Movie} />
+          <Route path="/" component={Home} exact />
+          <Route path = "*" component={Page404}/>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+
+export default App;
